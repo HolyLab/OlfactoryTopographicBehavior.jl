566a10d9f4445bb7ca608f8937199da2f2b1ec9c
Tim Holy <tim.holy@gmail.com>
2021-07-10 06:51:03 -0500

    Switch to carrier-signal lick detection
    The original file I developed this with had something wrong with the
    raw lick channel.

--- test/runtests.jl
+++ test/runtests.jl
@@ -6,23 +6,23 @@ using Test
 
 @testset "OlfactoryTopographicBehavior.jl" begin
     # TrialData extraction
-    df = DataFrame("soundlick"     => [0, 0, 0, 1, 1, 1,    1,    0, 0,     0, 0, 0, 0,     0,     0],
-                   "lickometer"    => [0, 0, 0, 0, 0, -0.3, -0.1, 0, -0.25, 0, 0, 0, -0.22, -0.03, 0],
-                   "odortiming"    => [0, 0, 0, 0, 0, 0,    0,    0, 4,     4, 0, 0, 0,     0,     0],
-                   "odordirection" => [1, 2, 2, 2, 2, 2,    2,    2, 2,     2, 2, 1, 1,     1,     1])
+    df = DataFrame("soundlick"     => [0, 0, 0, 1, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0],
+                   "lickometer"    => [1,-1, 1,-1, 1, 0, 0, 0, 1,-1, 1, 0, 1,-1, 1],
+                   "odortiming"    => [0, 0, 0, 0, 0, 0, 0, 0, 4, 4, 0, 0, 0, 0, 0],
+                   "odordirection" => [1, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 1, 1, 1, 1])
     fs = 2Hz
-    tds = trialdatas(df; lohi_sound = (0.25, 0.75), fs, soundduration=2s)
+    tds = trialdatas(df; lohi_sound = (0.25, 0.75), fs, soundduration=2s, fcarrierlick=1Hz, minlickduration=1s)
     td = only(tds)
     @test td.trialrange == 2:length(df.soundlick)
     @test td.soundinterval == 1s .. 3s
-    @test length(td.lickonsets) == 3
-    foreach(td.lickonsets) do t
-        idx = Int(t*fs + first(td.trialrange))
-        @test df.lickometer[idx] < -0.2
-    end
+    @test only(td.lickonsets) == 2s
     @test td.odordirection == 2
     @test td.odorinterval == 3.5s .. 4.5s
-    @test_throws ErrorException("soundlick pulse of duration 2.0 s starting at 4 not recognized") trialdatas(df; lohi_sound = (0.25, 0.75), fs, soundduration=3s)
+    tds = trialdatas(df; lohi_sound = (0.25, 0.75), fs, soundduration=2s, fcarrierlick=1Hz, minlickduration=0s)
+    td = only(tds)
+    @test td.lickonsets == [2s, 5s]
+    @test_throws ErrorException("peak frequency 1.0 Hz does not match expected 60 Hz") trialdatas(df; lohi_sound = (0.25, 0.75), fs, soundduration=3s)
+    @test_throws ErrorException("soundlick pulse of duration 2.0 s starting at 4 not recognized") trialdatas(df; lohi_sound = (0.25, 0.75), fs, soundduration=3s, fcarrierlick=1Hz, minlickduration=1s)
 
     # Display
     io = IOBuffer()
